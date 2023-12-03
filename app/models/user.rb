9a6d92371af34d1f4903cc97421e0043900c7197
LLLbin <2506868437@qq.com>
2023-12-03 03:12:39 -0800

    update README

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :orders, dependent: :destroy
 
   validates :username, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :role, presence: true
 
   after_create :create_cart
